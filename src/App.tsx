5fc2a2c9aee34d9555480cad4ad6325d009ae279
Robert Baruck <robert.baruck@sandstorm.de>
2022-11-23 16:37:09 +0100

    TASK: Routing & widget rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
 import './App.css'
-import Counter from './components/Counter'
-import CounterWithCustomHook from './components/CounterWithCustomHook'
-import HeaderAndText from './components/HeaderAndText'
-import HigherOrderComponent from './components/HigherOrderComponent'
-import MyList from './components/MyList'
-import ResizeDependendComponent from './components/ResizeDependendComponent'
-import TodoListWithCustomHook from './components/TodoList/TodoListWithCustomHook'
+import {Link, Outlet} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/* This is a comment in JSX */}
-      <HeaderAndText title='MyTitle' />
-
-      <HigherOrderComponent>
-        <h3>Composing Components</h3>
-        <MyList list={['one', 'two', 'three', 'four']} />
-      </HigherOrderComponent>
-      
-      <HigherOrderComponent otherComponent={<div>DEF</div>} children={<h3>Children as a prop</h3>} />
-
-      <Counter />
-
-      <ResizeDependendComponent />
-
-      <CounterWithCustomHook initialValue={0} />
-      <CounterWithCustomHook initialValue={100} stepSize={10} />
-
-      ########
-
-      <TodoListWithCustomHook />
+      <aside>
+        <nav>
+          <Link to={"/"}>Home</Link>
+          <Link to={"/todoList"}>TodoList</Link>
+          <Link to={"/counter"}>Counter</Link>
+        </nav>
+      </aside>
+      <main>
+        <Outlet />
+      </main>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,52 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App'
+import Counter from './components/Counter';
+import CounterWithCustomHook from './components/CounterWithCustomHook';
+import TodoListWithCustomHook from './components/TodoList/TodoListWithCustomHook';
 import './index.css'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "counter",
+        element: <Counter />,
+      },
+      {
+        path: "/todoList",
+        element: <TodoListWithCustomHook />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 )
+
+/***
+ * Example for small widgets in HTML rendered by CMS with data in HTML
+ */
+const Widget = (props: {count: number, step: number}) => {
+  console.log(props)
+
+  return (
+    <CounterWithCustomHook initialValue={props.count} stepSize={props.step} />
+  )
+}
+
+// detecte und hole counter widget
+const counterWidgets = document.querySelectorAll('[data-widget-id="Counter"]')
+counterWidgets.forEach(widget => {
+  const widgetData = JSON.parse(widget.getAttribute('data-widget-data')!) as {count: number, step: number}
+
+  ReactDOM.createRoot(widget).render(
+    <Widget count={widgetData.count} step={widgetData.step} />
+  )
+})
